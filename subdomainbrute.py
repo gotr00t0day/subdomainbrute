b52bf30bcffdc95c240b2a535d1845ad5db98257
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2020-06-08 16:02:29 -0400

    Create subdomainbrute.py

---
+++ subdomainbrute.py
@@ -0,0 +1,19 @@
+import requests
+from fake_useragent import UserAgent
+from colorama import Fore, Back, Style
+
+host = "reddit.com"
+wordlist = "wordlist.txt"
+try:
+    with open(wordlist, 'r') as f:
+        datalist = f.readlines()
+except IOError:
+    print("File not found!")
+
+ua = UserAgent()
+header = {'User-Agent':str(ua.chrome)}
+for subs in datalist:
+    subslist = subs.strip()
+    r = requests.get("https://" + subslist + "." + host, headers=header)
+    if r.status_code == 200: 
+       print(Fore.GREEN + subslist + "." + host)
